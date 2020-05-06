5195929fff4d711dc8a5573a3074ce566d9ee792
jbosworth20 <jbosworth@umass.edu>
2020-05-06 11:47:23 -0400

    All Scenes in Together

--- Assets/cookie.cs
+++ Assets/cookie.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class cookie : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+ 
+
+
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name == "Cat") {
+  
+        }
+    }
 }
