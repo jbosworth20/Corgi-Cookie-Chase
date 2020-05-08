cf49d1eddb2f0052704ce9938454cc2e6b71f50c
BoltyDawg <boltydawg@gmail.com>
2020-05-08 13:07:19 -0400

    auto generating sceene for hard difficulty

---
+++ Assets/LoadHouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LoadHouse : MonoBehaviour {
+
+	//public NavMeshSurface surface;
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+		List<GameObject> rooms = GameObject.FindGameObjectsWithTag("Room").ToList();
+		System.Random rnd = new System.Random();
+
+		foreach (GameObject point in spawnPoints) {
+			int i = rnd.Next(0, rooms.Count()-1);
+			Instantiate(rooms[i], point.transform.position, point.transform.rotation);
+			rooms.RemoveAt(i);
+		}
+	}
+}
