4a5a8fee17d2c76aa6c382e8a1625a7b92c6681f
BoltyDawg <boltydawg@gmail.com>
2020-05-09 09:24:33 -0400

    add cookie instantiating and animation

--- Assets/LoadHouse.cs
+++ Assets/LoadHouse.cs
@@ -6,6 +6,9 @@ using System.Linq;
 public class LoadHouse : MonoBehaviour {
 
 	//public NavMeshSurface surface;
+	public GameObject cookie;
+
+	private int cookieCount;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +19,16 @@ public class LoadHouse : MonoBehaviour {
 		foreach (GameObject point in spawnPoints) {
 			int i = rnd.Next(0, rooms.Count()-1);
 			Instantiate(rooms[i], point.transform.position, point.transform.rotation);
+			rooms[i].SetActive(false);
 			rooms.RemoveAt(i);
 		}
+
+		cookieCount = 0;
+		GameObject[] cookiePoints = GameObject.FindGameObjectsWithTag("CookiePoint");
+		int j = rnd.Next(0, cookiePoints.Length-1);
+		Instantiate(cookie, cookiePoints[j].transform.position, cookie.transform.rotation);
+		//Instantiate(cookie, new Vector3(0, 4, 25), cookiePoints[j].transform.rotation);
+		cookieCount++;
+		print(cookiePoints[j].transform.position);
 	}
 }
