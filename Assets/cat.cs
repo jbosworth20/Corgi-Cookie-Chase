16f6433230d1a85f295e9583af6611571a30acfd
BoltyDawg <boltydawg@gmail.com>
2020-05-09 17:49:55 -0400

    fix jumping bug

--- Assets/cat.cs
+++ Assets/cat.cs
@@ -18,7 +18,7 @@ public class cat : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        isJumping = false;
+        //isJumping = false;
         movement_input = Vector3.zero;
         // walking_speed = 5.0f;
         // running_speed = 7.0f;
@@ -42,53 +42,50 @@ public class cat : MonoBehaviour
         movement_input.x = Input.GetAxisRaw("Horizontal") * speed;
         movement_input.z = Input.GetAxis("Vertical") * speed;
 
-        if (character.isGrounded)
+        print(movement_input.y);
+
+        if(movement_input.y <= 0)
         {
             //animator.SetBool("isJumping", false);
-            if (Input.GetKeyDown(KeyCode.Space) && movement_input.y <= 0)
-            {
-                print("JUMPED");
-                movement_input.y = jump_speed;
-            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    movement_input.y = jump_speed;
+                }
         }
         
         if (movement_input.y > 0)
         {
-            print("fall");
             movement_input.y -= gravity * Time.deltaTime;
             character.Move(Vector3.up * movement_input.y * Time.deltaTime);
             //animator.SetBool("isJumping", true);
         }
 
-        //print(movement_input);
         animator.SetFloat("movement_speed", (float) movement_input.z);
-        //print(animator.GetFloat("movement_speed"));
 
         if (movement_input.x != 0)
         {
             //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(0, 0, movement_input.z)), Time.deltaTime * 7);
-            //print(movement_input.x);
             transform.Rotate(Vector3.up, Time.deltaTime * rotation_speed * movement_input.x);
         }
 
         if (movement_input.z != 0)
         {
-            //print("WALKING");
             character.Move(transform.forward * movement_input.z * speed * Time.deltaTime);
             //Vector3 look_rot = Quaternion.LookRotation(new Vector3(movement_input.x, 0.0f, movement_input.z).normalized).eulerAngles;
             //animator.SetBool("isWalking", true);
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                //animator.SetBool("isRunning", true);
-            }
+            // if (Input.GetKey(KeyCode.LeftShift))
+            // {
+            //     animator.SetBool("isRunning", true);
+            // }
         }
-        else
-        {
-            print("NOT WALKING");
-            //animator.SetBool("isRunning", false);
-            //animator.SetBool("isWalking", false);
-        }
+        // else
+        // {
+        //     print("NOT WALKING");
+        //     //animator.SetBool("isRunning", false);
+        //     //animator.SetBool("isWalking", false);
+        // }
 
-        character.Move(new Vector3(0, movement_input.y, 0) * Time.deltaTime);
+        //character.Move(new Vector3(0, movement_input.y, 0) * Time.deltaTime);
     }
 }
